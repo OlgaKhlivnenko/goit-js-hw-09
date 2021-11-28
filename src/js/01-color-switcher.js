28b60c201e77a5f52f639feb2ccb31e4cae57081
OlgaKhlivnenko <xlivnenko@ukr.net>
2021-11-28 12:09:28 -0500

    28-11

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+
+const bodyEl = document.querySelector('body');
+
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+let timeoutId = null;
+
+function getRandomHexColor() {
+  return console.log(`#${Math.floor(Math.random() * 16777215).toString(16)}`);
+}
+
+btnStart.addEventListener(`click`, onChangeColor);
+
+function onChangeColor(evt) {
+    console.log(evt);
+    timeoutId = setInterval(() => {
+        const colorChange = getRandomHexColor();
+        bodyEl.style.backgroundColor = colorChange;
+        console.log(`dct hf,jnftn`)
+        btnStart.classList.add(`disabled`);
+    }, 1000);
+}
+
+btnStop.addEventListener(`click`, onStopChangeColor);
+
+function onStopChangeColor() {
+    clearInterval(timeoutId);
+}
