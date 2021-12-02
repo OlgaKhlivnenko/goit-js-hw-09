984ab60818c09431f00125798c76a3518709a802
OlgaKhlivnenko <xlivnenko@ukr.net>
2021-12-02 15:54:15 +0200

    02-12/1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,21 +4,46 @@ import "flatpickr/dist/flatpickr.min.css";
 const inputEl = document.querySelector(`input#datetime-picker`);
 const btnStart = document.querySelector('button[data-start]');
 const timerEl = document.querySelector('.timer');
-const timer = {
-  const start(){
-    const startTime = Date.now();
-console.log(startTime);
+
+
+btnStart.addEventListener(`click`, onStartTime);
+function onStartTime() {
+  console.log(`click start`);
+  const startTime = Date.now();
+  console.log(startTime);
     setInterval(() => {
       const currentTime = Date.now();
-console.log(currentTime);
+      const ms = startTime - currentTime;
+     
+      const { days, hours, minutes, seconds } = convertMs(ms);
+       console.log(`${days}::${hours}::${minutes}::${seconds}`);
     }, 1000);
-  }
+}
+function addLeadingZero(value){
+  return String(value).padStart(2, `0`);
 }
 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
-
-
+  return { days, hours, minutes, seconds };
+}
+function addLeadingZero(value){
+  return String(value).padStart(2, `0`);
+}
 
 // const date = Date.now();
 // console.log(date);
@@ -36,8 +61,7 @@ console.log(currentTime);
 // });
 // btnStart.addEventListener(`click`, onStartTime);
 
-// function onStartTime() {
-//     console.log(`click start`);
+
     
 // console.log(fp);
 // }
@@ -54,25 +78,8 @@ console.log(currentTime);
 
 
 
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
 
-//   // Remaining days
-//   const days = Math.floor(ms / day);
-//   // Remaining hours
-//   const hours = Math.floor((ms % day) / hour);
-//   // Remaining minutes
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-//   // Remaining seconds
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-//   return { days, hours, minutes, seconds };
-// }
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
